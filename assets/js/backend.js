256f3d40279f7a424b8873c187741fb09a773a2c
Julian Hochgesang <julian@hochgesang.de>
2022-06-27 16:07:13 +0200

    Add servers configuration

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,3 +1,23 @@
+// #################################################################
+// For testing only! Should be implemented in base js from Walter
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+function getConfig() {
+    document.querySelector('.config-prompt').style.display = "none";
+    document.querySelector('.config-content').style.display = "block";
+}
+function addServerWizard() {
+    document.querySelector('.server-modal').style.display = "block";
+}
+var sparkConfig = {
+    editServer() {
+        document.querySelector('.server-modal').style.display = "block";
+    }
+};
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+// For testing only! Should be implemented in base js from Walter
+// #################################################################
+
+
 if ((cryear = document.getElementById("cryear")) !== null) {
     cryear.innerHTML = new Date().getFullYear();
 }
@@ -28,18 +48,6 @@ for (var i = 0; i < inlineCodes.length; i++) {
         }
     };
 }
-
-
-// #################################################################
-// For testing only! Should be implemented in base js from Walter
-// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-function getConfig() {
-    document.querySelector('.config-prompt').style.display = "none";
-    document.querySelector('.config-content').style.display = "block";
-}
-// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-// For testing only! Should be implemented in base js from Walter
-// #################################################################
 
 
 var tabs = document.getElementsByClassName('tab'), t;
@@ -82,6 +90,35 @@ for (var tb = 0; tb < tabButtons.length; tb++) {
             tabBC.classList.remove('active');
             if (tabBC.getAttribute('data-index') === currentTabButton.getAttribute('data-index')) {
                 tabBC.classList.add('active');
+            }
+        });
+    };
+}
+
+
+var serverModal = document.querySelector('.server-modal');
+if (serverModal) {
+    window.onclick = function(event) {
+        if (event.target == serverModal) {
+            serverModal.style.display = "none";
+        }
+     }
+}
+
+
+var radios = document.getElementsByName('protocolVal'), r;
+var radioContents = document.getElementsByClassName('server-modal-config-inner'), rc;
+for (var rc = 0; rc < radioContents.length; rc++) {
+    radioContents[rc].setAttribute('data-index', [rc]);
+}
+for (var r = 0; r < radios.length; r++) {
+    radios[r].setAttribute('data-index', [r]);
+    radios[r].onclick = function(){
+        var currentRadio = this;
+        Array.prototype.forEach.call(radioContents, radioC => {
+            radioC.classList.remove('active');
+            if (radioC.getAttribute('data-index') === currentRadio.getAttribute('data-index')) {
+                radioC.classList.add('active');
             }
         });
     };
